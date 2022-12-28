d16a7d8917682f14a44621a06ba004e469dbd163
JonathanJacobson <jonathanjac@gmail.com>
2022-12-28 09:53:59 +0000

    Initial commit Java Shortcuts

---
+++ src/com/foundations/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.foundations;
+
+public class HelloWorld
+{
+    public static void main(String[] args)  //psvm
+    {
+        System.out.println("HelloWorld!"); //sout
+        System.out.println("!");
+    }
+}
